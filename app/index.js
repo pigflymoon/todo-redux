9d6f81fbf7507c976f49ced56c496a920f023486
pigflymoon <pplflymoon2015@gmail.com>
2017-05-02 20:38:34 +1200

    # ”stage-0"是对ES7一些提案的支持，Babel通过插件的方式引入，让Babel可以编译ES7代码。

--- app/index.js
+++ app/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import App from './components/App';
+import reducer from './reducers';
 
-import Hello from './component/Hello';
+const store  = createStore(reducer);
 
-ReactDOM.render(
-  <Hello />,
-  document.getElementById('root')
-);
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
+
